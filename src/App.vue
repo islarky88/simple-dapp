b5f74d884d7076fe4257621e2a27b55d43ecee4c
Islarky Overlord <islarky88@gmail.com>
2021-05-30 02:28:58 +0800

    metamask connection success

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,77 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <v-card class="pa-3">
+          <h2 class="text-center">Simple Dapp</h2>
+
+          <v-btn
+            v-if="metamaskConnected"
+            @click="connectMetamask()"
+            class="mt-4"
+            color="primary"
+          >
+            Connect Wallet
+          </v-btn>
+          <v-btn
+            v-if="!metamaskConnected"
+            @click="disconnectMetamask()"
+            class="mt-4"
+            color="primary"
+          >
+            Disconnect Wallet
+          </v-btn>
+
+          <div></div>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Web3 from "web3";
 
 export default {
   name: "App",
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      metamaskConnected: false,
+    };
   },
+  mounted() {
+    this.initialize();
+  },
+  methods: {
+    async initialize() {
+      // check if metamask is installed
+      if (typeof window.ethereum !== "undefined") {
+        return;
+      }
 
-  data: () => ({
-    //
-  }),
+      console.log("MetaMask is installed!");
+
+      console.log("ethereum", window.ethereum);
+      var web3 = new Web3("ws://localhost:8546");
+      console.log(web3);
+      console.log("ethereum", window.ethereum);
+
+      console.log();
+    },
+    async connectMetamask() {
+      //Will Start the metamask extension
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = accounts[0];
+      if (account) {
+        this.metamaskConnected = true;
+      }
+      console.log("accounts", accounts, account);
+    },
+    async disconnectMetamask() {
+      console.log("disconnect");
+    },
+  },
 };
 </script>
